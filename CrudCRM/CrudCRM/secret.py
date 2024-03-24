c4e88cb0f8a507340a1d9f41a9025333a10119e8
Chisomloius <chisanloius@gmail.com>
2024-03-24 16:43:54 +0100

    standard files

---
+++ CrudCRM/CrudCRM/secret.py
@@ -0,0 +1,7 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+# SECRET_KEY = 'django-insecure-3q*8*5)^tvd!pmhr3!@95^*6+tr6$23#2u))!kurc*y=wb5-cz'
+class SecretKey():
+
+     def __str__(self):
+
+        return 'django-insecure-3q*8*5)^tvd!pmhr3!@95^*6+tr6$23#2u))!kurc*y=wb5-cz'
